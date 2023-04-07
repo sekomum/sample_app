c8baf7bf93624d9df5c3f00c56b0ae38dadd426a
sekomum <129589748+sekomum@users.noreply.github.com>
2023-04-07 09:31:44 +0000

    [Finish]3−7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -29,6 +29,12 @@ class ListsController < ApplicationController
     list.update(list_params)
     redirect_to list_path(list.id)  
   end
+  
+  def destroy
+    list = List.find(params{:id})
+    list.destory
+    redirect_to '/lists'
+  end
     
   private
   
